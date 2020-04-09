c29220e61351321670ee7a86cf35d4980312bded
Lucas <lukinhasmlopes@hotmail.com>
2020-04-09 00:04:10 -0300

    Updated failure streak message

--- src/br/com/craftlife/creative/dailylogin/core/MessagesManager.java
+++ src/br/com/craftlife/creative/dailylogin/core/MessagesManager.java
@@ -48,12 +48,11 @@ public class MessagesManager {
 		int box = config.getInt("prizes.day1.box.qntd");
 		int boxlvl = config.getInt("prizes.day1.box.lvl");
 		p.sendMessage("§b§lSeja bem-vindo ao §a§lCraft§e§lLife§b§l!");
-		p.sendMessage("§cVocê ficou um tempo sem logar e");
-		p.sendMessage("§cperdeu sua sequência de dias :c");
 		p.sendMessage("§bPor logar, você recebeu §2"+config.getInt("prizes.day1.money")+" Money §be §5"+config.getInt("prizes.day1.dust")+"§5 Dusts");
 		if (box>0) {
 			p.sendMessage("§bAlém de §d"+box+"§5 Caixa Misteriosa "+Util.StarsCheck(boxlvl));
 		}
-		p.sendMessage("§bLogue todo dia e receba ainda mais prêmios!");
+		p.sendMessage("§cVocê ficou um tempo sem logar e");
+		p.sendMessage("§cperdeu sua sequência de dias :c");
 	}
 }
